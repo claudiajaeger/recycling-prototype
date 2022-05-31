8cfdd8de6aac997e37861c307bd89d75409b5c07
Selina Öberg <43641922+selinaoberg@users.noreply.github.com>
2022-05-31 13:29:26 +0200

    Cleaned up code

--- index.py
+++ index.py
@@ -5,15 +5,10 @@ import urllib.request
 from werkzeug.utils import secure_filename
 import os
 
-# Trying out the function
-# pred_label = getPrediction('plastic/plastic25.jpg')
-# print(pred_label)
-
 UPLOAD_FOLDER = 'static/'
-# Full dir: /Users/selinaoberg/Documents/Universitetet/VT-22/Exjobb/Waste_Classificator/uploads/
 
 app = Flask(__name__)                    
-app.secret_key = '8662747133' # change value?
+app.secret_key = '8662747133'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Route to HTML
@@ -21,7 +16,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def index():
     return render_template('index.html')
 
-@app.route("/", methods = ['POST']) #/file
+@app.route("/", methods = ['POST'])
 # Our function for pushing the image to the classifier model
 def submit_image():
      if request.method == 'POST':
@@ -36,14 +31,11 @@ def submit_image():
         # Return results predictive data
           if file:
             filename = secure_filename(file.filename)
-            # kanske behöver byta till actual dir istället för UPLOAD_FOLDER
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename)) 
-            # getPrediction(filename)
             prediction, filename, transform = getPrediction(filename)
             flash(prediction)
             flash(filename)
             flash(transform)
-            # need to flash(filename) too?
             return redirect('/')
 
 if __name__ == "__main__":
